e5f653a192142172ed5b177edbb3988acd97a96e
Aman Singh <amansingh17112004@gmail.com>
2025-07-05 12:35:30 +0530

    Create (Difficulty:Easy) Max Score from Subarray Mins.java

---
+++ GeeksforGeeks/(Difficulty:Easy) Max Score from Subarray Mins.java
@@ -0,0 +1,12 @@
+// https://www.geeksforgeeks.org/problems/max-sum-in-sub-arrays0824/1
+
+class Solution {
+    public int maxSum(int arr[]) {
+        // code here
+        int max=0;
+        for(int i=0;i<arr.length-1;i++){
+            max=Math.max(max,arr[i]+arr[i+1]);
+        }
+        return max;
+    }
+}
