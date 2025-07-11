d9684450faf5daf731b9300a4244e6166164c9a9
Aman Singh <amansingh17112004@gmail.com>
2025-07-11 18:31:42 +0530

    Create (Difficulty:1205) Drumpf for President!.java

---
+++ CodeChef/(Difficulty:1205) Drumpf for President!.java
@@ -0,0 +1,36 @@
+// https://www.codechef.com/problems/STUDVOTE
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    int k=input.nextInt();
+		    int[] arr=new int[n];
+		    int[] freq=new int[n+1];
+		    for(int i=0;i<n;i++){
+		        arr[i]=input.nextInt();
+		        freq[arr[i]]++;
+		    }
+		    int count=0;
+		    for(int i=0;i<n;i++){
+		        if(arr[i]==i+1){
+		            freq[arr[i]]=0;
+		        }
+		    }
+		    for(int i=0;i<n+1;i++){
+		        if(freq[i]>=k) count++;
+		    }
+		    System.out.println(count);
+		}
+
+	}
+}
