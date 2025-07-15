84a8567f0b4aa33bf5e63cdd3c03ed443c14c13d
Aman Singh <amansingh17112004@gmail.com>
2025-07-15 10:46:37 +0530

    Create (Difficulty.Medium) Ways to Reach the n'th Stair.java

---
+++ GeeksforGeeks/(Difficulty.Medium) Ways to Reach the n'th Stair.java
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/problems/count-ways-to-reach-the-nth-stair-1587115620/1?page=1&category=Mathematical&status=unsolved&sortBy=submissions
+
+class Solution {
+    int countWays(int n) {
+        if(n==0|| n==1) return 1;
+        int a=1,b=1;
+        for(int i=2;i<=n;i++) {
+            int temp=a+b;
+            a=b;
+            b=temp;
+        }
+        return b;
+    }
+}
