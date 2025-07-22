57f46fb637c06d169cdf93cdc829dcb1e671af8a
Aman Singh <amansingh17112004@gmail.com>
2025-07-22 08:00:17 +0530

    Create SingleOperationPart1_Easy.java

---
+++ CodeChef/SingleOperationPart1_Easy.java
@@ -0,0 +1,26 @@
+// https://www.codechef.com/problems/SINGLEOP1?tab=statement
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    String s=input.next();
+		    int count=0;
+		    for(int i=0;i<n;i++){
+		        if(s.charAt(i)=='1') count++;
+		        else break;
+		    }
+		    System.out.println(count);
+		}
+
+	}
+}
