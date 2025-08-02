01f1a6aa4eb9fa6ffc4626d5961eabd9cdea4ee0
Aman Singh <amansingh17112004@gmail.com>
2025-08-02 14:29:17 +0530

    Create MinimumMovesToEqualArrayElements_Medium.java

---
+++ LeetCode/MinimumMovesToEqualArrayElements_Medium.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/minimum-moves-to-equal-array-elements/?envType=problem-list-v2&envId=n32iyzu3
+
+class Solution {
+    public int minMoves(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        long sum=0;
+        for(int num:nums){
+            sum+=num;
+            min=Math.min(min, num);
+        }
+        return (int)(sum-(long)min*nums.length);
+    }
+}
