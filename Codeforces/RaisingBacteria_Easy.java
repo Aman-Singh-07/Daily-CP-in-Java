2527689ee66dc3ebb45d2b928a13adf34b66203b
Aman Singh <amansingh17112004@gmail.com>
2025-08-14 14:59:21 +0530

    Create RaisingBacteria_Easy.java

---
+++ Codeforces/RaisingBacteria_Easy.java
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/579/A
+
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        long n=input.nextLong();
+        System.out.println(Long.bitCount(n));
+    }
+}
