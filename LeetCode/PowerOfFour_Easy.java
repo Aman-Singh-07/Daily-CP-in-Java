c010dbfebcd2eff592f09cc57665a6e345287855
Aman Singh <amansingh17112004@gmail.com>
2025-08-15 10:25:42 +0530

    Create PowerOfFour_Easy.java

---
+++ LeetCode/PowerOfFour_Easy.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/power-of-four/
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        return n>0 && (n&(n-1))==0 && (n&0x55555555)!=0;
+        
+    }
+}
