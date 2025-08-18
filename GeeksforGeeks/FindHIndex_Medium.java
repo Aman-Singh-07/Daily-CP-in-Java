c2d88c1bfc2f6bbf27d780b718ba17df99a432ff
Aman Singh <amansingh17112004@gmail.com>
2025-08-18 17:34:29 +0530

    Create FindHIndex_Medium.java

---
+++ GeeksforGeeks/FindHIndex_Medium.java
@@ -0,0 +1,17 @@
+// https://www.geeksforgeeks.org/problems/find-h-index--165609/1
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n=citations.length;
+        int h=0;
+        for(int i=0;i<n;i++){
+            int count=n-i;
+            if(citations[i]>=count){
+                h=count;
+                break;
+            }
+        }
+        return h;
+    }
+}
