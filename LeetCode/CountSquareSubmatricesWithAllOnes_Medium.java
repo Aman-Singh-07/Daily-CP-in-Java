8e2c81a437ef4e8763e335a1a4bcb1f3691fba66
Aman Singh <amansingh17112004@gmail.com>
2025-08-20 11:37:32 +0530

    Create CountSquareSubmatricesWithAllOnes_Medium.java

---
+++ LeetCode/CountSquareSubmatricesWithAllOnes_Medium.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/count-square-submatrices-with-all-ones/description/?envType=daily-question&envId=2025-08-20
+
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m=matrix.length, n=matrix[0].length;
+        int res=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==1 && i>0 && j>0){
+                    matrix[i][j]=Math.min(
+                        Math.min(matrix[i-1][j],matrix[i][j-1]),
+                        matrix[i-1][j-1]
+                    )+1;
+                }
+                res+=matrix[i][j];
+            }
+        }
+        return res;
+    }
+}
