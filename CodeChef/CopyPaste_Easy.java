ca750fa3cab81bae18d89756f786022a2aa10736
Aman Singh <amansingh17112004@gmail.com>
2025-08-24 10:54:45 +0530

    Create CopyPaste_Easy.java

---
+++ CodeChef/CopyPaste_Easy.java
@@ -0,0 +1,24 @@
+// https://www.codechef.com/problems/RRCOPY?tab=ide
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    HashSet<Integer> set=new HashSet<>();
+		    for(int i=0;i<n;i++){
+		        set.add(input.nextInt());
+		    }
+		    System.out.println(set.size());
+		}
+
+	}
+}
