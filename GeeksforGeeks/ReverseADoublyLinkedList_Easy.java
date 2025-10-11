7cadd60fcbfb2f041e868e97821971d9c475b697
Aman Singh <amansingh17112004@gmail.com>
2025-10-11 10:38:12 +0530

    Create ReverseADoublyLinkedList_Easy.java

---
+++ GeeksforGeeks/ReverseADoublyLinkedList_Easy.java
@@ -0,0 +1,34 @@
+// https://www.geeksforgeeks.org/problems/reverse-a-doubly-linked-list/1?page=5&status=unsolved&sortBy=submissions
+
+/*
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    public Node reverse(Node head) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        Node ptr=head;
+        while(ptr!=null){
+            list.add(ptr.data);
+            ptr=ptr.next;
+        }
+        ptr=head;
+        int k=list.size()-1;
+        while(ptr!=null){
+            ptr.data=list.get(k);
+            k--;
+            ptr=ptr.next;
+        }
+        return head;
+    }
+}
