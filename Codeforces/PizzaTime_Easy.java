cc83482500c4c8d390149c37b622a30f20570804
Aman Singh <amansingh17112004@gmail.com>
2025-10-25 15:06:35 +0530

    Create PizzaTime_Easy.java

---
+++ Codeforces/PizzaTime_Easy.java
@@ -0,0 +1,14 @@
+// https://codeforces.com/contest/2156/problem/A
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int t=input.nextInt();
+        while(t-->0){
+            long n=input.nextLong();
+            System.out.println((n-1)/2);
+
+        }
+    }
+}
