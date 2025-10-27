ad8ee17c4448b32308f9db1978dc8965c5eeb1c5
Aman Singh <amansingh17112004@gmail.com>
2025-10-27 14:00:16 +0530

    Create NumberOfLaserBeamsInABank_Medium.java

---
+++ LeetCode/NumberOfLaserBeamsInABank_Medium.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/number-of-laser-beams-in-a-bank/description/?envType=daily-question&envId=2025-10-27
+
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int[] arr=new int[bank.length];
+        for(int i=0;i<bank.length;i++){
+            int count=0;
+            for(char ch:bank[i].toCharArray()){
+                if(ch=='1') count++;
+            }
+            arr[i]=count;
+        }
+        int res=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                for(int j=i+1;j<arr.length;j++){
+                    if(arr[j]!=0){
+                        res+=arr[i]*arr[j];
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+        
+
+    }
+}
