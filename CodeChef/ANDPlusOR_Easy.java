81eeef7119675979cdde4169e1c3e735825993ff
Aman Singh <amansingh17112004@gmail.com>
2025-10-28 09:17:00 +0530

    Create ANDPlusOR_Easy.java

---
+++ CodeChef/ANDPlusOR_Easy.java
@@ -0,0 +1,7 @@
+//  https://www.codechef.com/problems/ANDOR?tab=statement
+
+public static void solve(long x) {
+        // write your code here 
+        if(x==0) System.out.println("-1");
+        else System.out.println(0+" "+x);
+}
