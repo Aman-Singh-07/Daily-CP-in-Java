65cb898505fad37d9b6796c53fcc28a1f88440f4
Aman Singh <amansingh17112004@gmail.com>
2025-11-05 10:44:29 +0530

    Create MinimumNumberOfOperationsToMoveAllBallsToEachBox_Medium.java

---
+++ LeetCode/MinimumNumberOfOperationsToMoveAllBallsToEachBox_Medium.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
+
+class Solution {
+    public int[] minOperations(String boxes) {
+        int[] ans=new int[boxes.length()];
+        for(int i=0;i<boxes.length();i++){
+            int sum=0;
+            for(int j=0;j<boxes.length();j++){
+                if(boxes.charAt(j)=='1'){
+                    sum+=Math.abs(i-j);
+                }
+            }
+            ans[i]=sum;
+        }
+        return ans;
+    }
+}
