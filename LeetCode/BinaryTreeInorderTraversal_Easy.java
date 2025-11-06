8ba28db57dd3e803f04cb09876d70e953e332149
Aman Singh <amansingh17112004@gmail.com>
2025-11-06 16:12:10 +0530

    Create BinaryTreeInorderTraversal_Easy.java

---
+++ LeetCode/BinaryTreeInorderTraversal_Easy.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        find(root,res);
+        return res;
+    }
+
+    public void find(TreeNode root,List<Integer> res){
+        if(root!=null){
+            find(root.left,res);
+            res.add(root.val);
+            find(root.right,res);
+        }
+    }
+}
