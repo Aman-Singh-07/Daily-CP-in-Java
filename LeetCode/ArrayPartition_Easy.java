499310956970f60ef431a1882203ae37673cc667
Aman Singh <amansingh17112004@gmail.com>
2025-11-14 10:44:14 +0530

    Create ArrayPartition_Easy.java

---
+++ LeetCode/ArrayPartition_Easy.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/array-partition/description/
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res=0;
+        for(int i=nums.length-2;i>=0;i-=2){
+            res+=nums[i];
+        }
+        return res;
+    }
+}
