c25018d1a5b2e81ac8a93ab78cf043468f05c5c3
Aman Singh <amansingh17112004@gmail.com>
2025-11-22 17:10:01 +0530

    Create FindMinimumOperationsToMakeAllElementsDivisibleByThree_Easy.java

---
+++ LeetCode/FindMinimumOperationsToMakeAllElementsDivisibleByThree_Easy.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/?envType=daily-question&envId=2025-11-22
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int count=0;
+        for(int num:nums){
+            if(num%3!=0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
