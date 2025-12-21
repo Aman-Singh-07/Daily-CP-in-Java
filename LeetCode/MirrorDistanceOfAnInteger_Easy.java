5ff0c5a6c2d6b0b84c0b7a2eab4f8bf9935f0210
Aman Singh <amansingh17112004@gmail.com>
2025-12-21 09:37:35 +0530

    Create MirrorDistanceOfAnInteger_Easy.java

---
+++ LeetCode/MirrorDistanceOfAnInteger_Easy.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/mirror-distance-of-an-integer/description/
+
+class Solution {
+    public int mirrorDistance(int n) {
+        return Math.abs(n-reverse(n));
+    }
+
+    public static int reverse(int n){
+        int rev=0;
+        while(n>0){
+            int r=n%10;
+            rev=rev*10+r;
+            n/=10;
+        }
+        return rev;
+    }
+}
