86817ffa5d29c53ef866a7b860db27d11d9c00e0
Aman Singh <amansingh17112004@gmail.com>
2025-12-27 10:03:51 +0530

    Create NumberGameRevisited_Easy.java

---
+++ CodeChef/NumberGameRevisited_Easy.java
@@ -0,0 +1,21 @@
+// https://www.codechef.com/problems/NUMGAME2?tab=statement
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    if(n%4==1) System.out.println("ALICE");
+		    else System.out.println("BOB");
+		}
+
+	}
+}
