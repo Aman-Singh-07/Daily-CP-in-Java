f8f35f529ea622c28bbc1ee78e5b93b9a221d637
Aman Singh <amansingh17112004@gmail.com>
2026-01-15 13:40:19 +0530

    Create ReversePrefixOfWord_Easy.java

---
+++ LeetCode/ReversePrefixOfWord_Easy.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/reverse-prefix-of-word/description/
+
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int index=word.indexOf(ch);
+        if (index==-1) return word;
+        StringBuilder sb=new StringBuilder(word.substring(0,index+1));
+        sb.reverse();
+        return sb.toString() + word.substring(index+1);
+    }
+}
