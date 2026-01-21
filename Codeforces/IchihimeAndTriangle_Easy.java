d8abe3972ec04789b8e193e0e66c1ebd44e67391
Aman Singh <amansingh17112004@gmail.com>
2026-01-21 19:11:48 +0530

    Create IchihimeAndTriangle_Easy.java

---
+++ Codeforces/IchihimeAndTriangle_Easy.java
@@ -0,0 +1,17 @@
+// https://codeforces.com/problemset/problem/1337/A
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int t=input.nextInt();
+        while(t-->0){
+            long a=input.nextLong();
+            long b=input.nextLong();
+            long c=input.nextLong();
+            long d=input.nextLong();
+            System.out.println(b+" "+c+" "+c);
+        }
+    }
+}
