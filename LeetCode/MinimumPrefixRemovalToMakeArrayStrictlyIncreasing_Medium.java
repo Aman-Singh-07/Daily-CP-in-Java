d0b9b044a1d4c0228b5768d566f75f9c19d05302
Aman Singh <amansingh17112004@gmail.com>
2026-01-25 09:53:35 +0530

    Create MinimumPrefixRemovalToMakeArrayStrictlyIncreasing_Medium.java

---
+++ LeetCode/MinimumPrefixRemovalToMakeArrayStrictlyIncreasing_Medium.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-prefix-removal-to-make-array-strictly-increasing/description/
+
+class Solution {
+    public int minimumPrefixLength(int[] nums) {
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i]<=nums[i-1]) return i;
+        }
+        return 0;
+    }
+}
