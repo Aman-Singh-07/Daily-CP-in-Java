27abef2405ad40d22c0e4f40c6ca38d279146512
Aman Singh <amansingh17112004@gmail.com>
2026-02-21 17:09:27 +0530

    Create RemovingConsecutiveDuplicates2_Easy.java

---
+++ GeeksforGeeks/RemovingConsecutiveDuplicates2_Easy.java
@@ -0,0 +1,22 @@
+// https://www.geeksforgeeks.org/problems/removing-consecutive-duplicates-2-1587115621/1
+
+class Solution {
+    public static String removePair(String s) {
+        // code here
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(stack.isEmpty() || stack.peek()!=ch) stack.push(ch);
+            else{
+                if(stack.peek()==ch){
+                    stack.pop();
+                }
+            }
+        }
+        char[] ch=new char[stack.size()];
+        int i=ch.length-1;
+        while(!stack.isEmpty()){
+            ch[i--]=stack.pop();
+        }
+        return new String(ch);
+    }
+}
