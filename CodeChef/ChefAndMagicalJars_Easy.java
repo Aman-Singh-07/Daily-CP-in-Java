9b4142132f8ff518e207e35cbeda02d7d5cde933
Aman Singh <amansingh17112004@gmail.com>
2026-02-22 08:55:02 +0530

    Create ChefAndMagicalJars_Easy.java

---
+++ CodeChef/ChefAndMagicalJars_Easy.java
@@ -0,0 +1,25 @@
+// https://www.codechef.com/problems/MAGICJAR?tab=statement
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    long sum=0;
+		    for(int i=0;i<n;i++){
+		        sum+=input.nextLong();
+		    }
+		    sum++;
+		    System.out.println(sum-n);
+		}
+
+	}
+}
