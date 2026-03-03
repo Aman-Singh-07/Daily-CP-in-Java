f580296b9b99b560967108e5873f970638efc783
Aman Singh <amansingh17112004@gmail.com>
2026-03-03 14:43:33 +0530

    Create FindKthBitInNthBinaryString_Medium.java

---
+++ LeetCode/FindKthBitInNthBinaryString_Medium.java
@@ -0,0 +1,17 @@
+//  https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/?envType=daily-question&envId=2026-03-03
+
+class Solution {
+    public char findKthBit(int n, int k) {
+        if(n==1) return '0';
+        int length=(1<<n)-1; 
+        int mid=length/2+1;
+        if(k==mid){
+            return '1';
+        }else if(k<mid){
+            return findKthBit(n-1,k);
+        }else{
+            char bit=findKthBit(n-1,length-k+1);
+            return (bit=='0')?'1':'0';
+        }
+    }
+}
