33b9a386a73c931125c54b1c2558a154b5b9b19b
Aman Singh <amansingh17112004@gmail.com>
2026-03-07 13:34:38 +0530

    Create ConstructBinaryTreeFromParentArray_Medium.java

---
+++ GeeksforGeeks/ConstructBinaryTreeFromParentArray_Medium.java
@@ -0,0 +1,34 @@
+// https://www.geeksforgeeks.org/problems/construct-binary-tree-from-parent-array/1
+
+
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    Node createTree(int[] parent) {
+        // code here
+        int n=parent.length;
+        Node[] nodes=new Node[n];
+        Node root=null;
+        for(int i=0;i<n;i++){
+            nodes[i]=new Node(i);
+        }
+        for(int i=0;i<n;i++){
+            if(parent[i]==-1) root=nodes[i];
+            else{
+                Node p=nodes[parent[i]];
+                if(p.left==null) p.left=nodes[i];
+                else p.right=nodes[i];
+            }
+        }
+        return root;
+        
+    }
+}
