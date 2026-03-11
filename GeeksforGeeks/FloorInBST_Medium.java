ede96948ae7853585dd196fe38d50fe63860fe5b
Aman Singh <amansingh17112004@gmail.com>
2026-03-11 23:33:56 +0530

    Add findFloor method in Solution class for BST
    Implement findFloor method to find the floor value of x in a BST.

---
+++ GeeksforGeeks/FloorInBST_Medium.java
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/implementing-floor-in-bst/1
+
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    public int findFloor(Node root, int x) {
+        // code here
+        if(root==null) return -1;
+        int res=-1;
+        while(root!=null){
+            if(root.data==x) return x;
+            if(root.data>x) root=root.left;
+            else if(root.data<x){
+                res=root.data;
+                root=root.right;
+            }
+        }
+        return res;
+    }
+}
