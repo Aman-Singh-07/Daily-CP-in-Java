7ec8600640b66c8edcfc6db5946171f35484ea8e
Aman Singh <amansingh17112004@gmail.com>
2026-03-12 14:41:41 +0530

    Create VacationExcitement_Easy.java

---
+++ CodeChef/VacationExcitement_Easy.java
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/VACAEX
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int x=input.nextInt();
+		int y=input.nextInt();
+		System.out.println(y+x-1);
+
+	}
+}
