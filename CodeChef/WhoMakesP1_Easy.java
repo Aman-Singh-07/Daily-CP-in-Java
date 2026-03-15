ef9d4015e9851792ddfbc636f059bbdd9fddffed
Aman Singh <amansingh17112004@gmail.com>
2026-03-15 19:33:39 +0530

    Create WhoMakesP1_Easy.java

---
+++ CodeChef/WhoMakesP1_Easy.java
@@ -0,0 +1,19 @@
+// https://www.codechef.com/problems/WHOMAKESP1?tab=statement
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int a=input.nextInt();
+		int b=input.nextInt();
+		System.out.println((a>b)?"Dom":"Tyro");
+
+	}
+}
+
