68e3a4a2fe824d98103166b8209bcf4029f06391
Aman Singh <amansingh17112004@gmail.com>
2026-03-26 09:54:29 +0530

    Create SnakyStrings_Easy.java

---
+++ CodeChef/SnakyStrings_Easy.java
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/SSNK
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		String s=input.next();
+		System.out.println((s.charAt(0)=='s' || s.charAt(3)=='s')?"Yes":"No");
+
+	}
+}
