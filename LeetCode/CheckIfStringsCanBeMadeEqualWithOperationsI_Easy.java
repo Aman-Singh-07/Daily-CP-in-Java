4c818e8ee8bc007eeb7d6ea1361447528f4f8628
Aman Singh <amansingh17112004@gmail.com>
2026-03-29 13:51:46 +0530

    Create CheckIfStringsCanBeMadeEqualWithOperationsI_Easy.java

---
+++ LeetCode/CheckIfStringsCanBeMadeEqualWithOperationsI_Easy.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/description/?envType=daily-question&envId=2026-03-29
+
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        boolean evenMatch = (s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+                            (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0));
+        boolean oddMatch = (s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+                           (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1));
+        
+        return evenMatch && oddMatch;
+    }
+}
