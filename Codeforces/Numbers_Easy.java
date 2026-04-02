5e5228e12d5cddc00778107bcd29f95d62ca4871
Aman Singh <amansingh17112004@gmail.com>
2026-04-02 16:18:24 +0530

    Create Numbers_Easy.java

---
+++ Codeforces/Numbers_Easy.java
@@ -0,0 +1,9 @@
+// https://codeforces.com/contest/2214/problem/F
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner input=new Scanner(System.in);
+      System.out.println("92136");
+    }
+}
