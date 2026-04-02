e4de1d5d1908b2043409cd8c5b7a3bb0cc1bef3f
Aman Singh <amansingh17112004@gmail.com>
2026-04-02 16:11:51 +0530

    Create SpecialProblem_Medium.java

---
+++ Codeforces/SpecialProblem_Medium.java
@@ -0,0 +1,10 @@
+// https://codeforces.com/contest/2214/problem/J
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        String s=input.next();
+        System.out.println("Yes, I can attest to my status as a thoroughly validated, carbon-based biological entity.");
+    }
+}
