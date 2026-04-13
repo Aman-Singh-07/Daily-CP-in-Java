cd5c81c60c883ff386e33c76493c66fd4375174d
Aman Singh <amansingh17112004@gmail.com>
2026-04-13 20:45:48 +0530

    Create MinimumDistanceToTheTargetElement_Easy.java

---
+++ LeetCode/MinimumDistanceToTheTargetElement_Easy.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/minimum-distance-to-the-target-element/description/?envType=daily-question&envId=2026-04-13
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target) min=Math.min(min,Math.abs(i-start));
+        }
+        return min;
+    }
+}
