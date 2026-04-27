73b9258ff9e295c01b0a3c1b8dadd29ef960155b
Aman Singh <amansingh17112004@gmail.com>
2026-04-27 23:59:18 +0530

    Create FindIndicesOfStableMountains_Easy.java

---
+++ LeetCode/FindIndicesOfStableMountains_Easy.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/find-indices-of-stable-mountains/description/
+
+class Solution {
+    public List<Integer> stableMountains(int[] height, int threshold) 
+    {
+        List<Integer> ans = new ArrayList<>();
+        for(int i=1;i<height.length;i++)
+        {
+            if(height[i-1]>threshold)
+            {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
