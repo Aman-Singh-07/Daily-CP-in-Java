4e659bddcad978c25606944c2dd537d5d6e3d71b
Aman Singh <amansingh17112004@gmail.com>
2026-05-05 19:07:01 +0530

    Create 1DEraser_Easy.java

---
+++ Codeforces/1DEraser_Easy.java
@@ -0,0 +1,30 @@
+// https://codeforces.com/problemset/problem/1873/D
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    int k=input.nextInt();
+		    String s=input.next();
+		    int i=0;
+		    int count=0;
+		    while(i<n){
+		        if(s.charAt(i)=='B'){
+		            count++;
+		            i+=k;
+		        }else i++;
+		    }
+		    System.out.println(count);
+		}
+
+	}
+}
