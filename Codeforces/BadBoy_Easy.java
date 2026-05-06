c8753b1599207d1155ac10187e0afbe1b09a29c5
Aman Singh <amansingh17112004@gmail.com>
2026-05-06 22:06:15 +0530

    Create BadBoy_Easy.java

---
+++ Codeforces/BadBoy_Easy.java
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/1537/B
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Code
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    int m=input.nextInt();
+		    int p=input.nextInt();
+		    int q=input.nextInt();
+		    System.out.println("1 1 "+n+" "+m);
+		}
+
+	}
+}
