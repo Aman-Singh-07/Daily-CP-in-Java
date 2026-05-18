db8de7a33d6c81f1135848288d2cc008f226a8c1
Aman Singh <amansingh17112004@gmail.com>
2026-05-18 16:38:58 +0530

    Create MaximumSumProblem_Easy.java

---
+++ GeeksforGeeks/MaximumSumProblem_Easy.java
@@ -0,0 +1,13 @@
+// https://www.geeksforgeeks.org/problems/maximum-sum-problem2211/1
+
+class Solution {
+    public int maxSum(int n) {
+        // code here.
+        if(n==0 || n==1) return n;
+        int a=maxSum(n/2);
+        int b=maxSum(n/3);
+        int c=maxSum(n/4);
+        return Math.max(n,a+b+c);
+        
+    }
+}
