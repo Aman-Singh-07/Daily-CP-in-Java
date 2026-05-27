959df3e76cd859799f3bb85e881000432c607084
Aman Singh <amansingh17112004@gmail.com>
2026-05-27 15:04:15 +0530

    Create PositiveOrNegativeSubarrays_Easy.java

---
+++ CodeChef/PositiveOrNegativeSubarrays_Easy.java
@@ -0,0 +1,26 @@
+// https://www.codechef.com/problems/POSITNEG?tab=statement
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0){
+		    int n=input.nextInt();
+		    long p=0;
+		    long q=0;
+		    for(int i=0;i<n;i++){
+		        int x=input.nextInt();
+		        if(x<0) q+=i+1;
+		        else p+=i+1;
+		    }
+		    System.out.println(Math.abs(p-q));
+		}
+	}
+}
