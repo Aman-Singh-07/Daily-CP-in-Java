a068be974a8d470bc5c23ed573e7e3fea24627d8
Aman Singh <amansingh17112004@gmail.com>
2026-06-07 15:46:35 +0530

    Create FindingProfession_Medium.java

---
+++ GeeksforGeeks/FindingProfession_Medium.java
@@ -0,0 +1,11 @@
+// https://www.geeksforgeeks.org/problems/finding-profession3834/1
+
+class Solution {
+    public String profession(int level, int pos) {
+        // code here
+        int count=Integer.bitCount(pos-1);
+        if(count%2==0) return "Engineer";
+        return "Doctor";
+        
+    }
+}
