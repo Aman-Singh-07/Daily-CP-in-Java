30ccff205f3d1ec7f65663489aeea94471464127
Aman Singh <amansingh17112004@gmail.com>
2026-06-11 00:24:00 +0530

    Update TwoSum_Easy.java

--- LeetCode/TwoSum_Easy.java
+++ LeetCode/TwoSum_Easy.java
@@ -2,16 +2,21 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr=new int[2];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int idx1=-1;
+        int idx2=-1;
         for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if(target==(nums[i]+nums[j]) && i!=j){
-                    arr[0]=i;
-                    arr[1]=j;
-                    break;
-                }
+            int num2=target-nums[i];
+            if(map.containsKey(num2)){
+                idx1=i;
+                idx2=map.get(num2);
+                break;
             }
+            map.put(nums[i],i);
         }
-        return arr; 
+        int[] res=new int[2];
+        res[0]=idx1;
+        res[1]=idx2;
+        return res;
     }
 }
