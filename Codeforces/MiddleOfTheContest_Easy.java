190b122f68cb7b4730085856f857da9ef5ffdaad
Aman Singh <amansingh17112004@gmail.com>
2026-06-13 13:37:53 +0530

    Create MiddleOfTheContest_Easy.java

---
+++ Codeforces/MiddleOfTheContest_Easy.java
@@ -0,0 +1,226 @@
+// https://codeforces.com/problemset/problem/1133/A
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws Exception {
+
+        int t = 1;
+
+        // Uncomment for multiple test cases
+        // t = fs.nextInt();
+
+        while (t-- > 0) {
+            solve();
+        }
+
+        out.flush();
+        out.close();
+    }
+
+    static void solve() {
+
+        // Write your solution here
+
+        String s1=fs.next();
+        String s2=fs.next();
+        String[] str1=s1.split(":");
+        String[] str2=s2.split(":");
+        int h1=Integer.valueOf(str1[0]);
+        int h2=Integer.valueOf(str2[0]);
+        int m1=Integer.valueOf(str1[1]);
+        int m2=Integer.valueOf(str2[1]);
+        int time1=h1*60+m1;
+        int time2=h2*60+m2;
+        int time=(time2-time1)/2;
+        time+=m1;
+        int reqH=h1+(time/60);
+        int reqM=(time%60);
+        String sRes="";
+        String mRes="";
+        if(reqH<=9) sRes="0";
+        if(reqM<=9) mRes="0";
+        out.println(sRes+reqH+":"+mRes+reqM);
+
+    }
+
+    /* ================= Utility Methods ================= */
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    static long power(long a, long b) {
+        long result = 1;
+
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                result *= a;
+            }
+
+            a *= a;
+            b >>= 1;
+        }
+
+        return result;
+    }
+
+    static long modPower(long a, long b, long mod) {
+        long result = 1;
+
+        a %= mod;
+
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                result = (result * a) % mod;
+            }
+
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+
+        return result;
+    }
+
+    static void sort(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int x : arr) {
+            list.add(x);
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+    }
+
+    static void reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    /* ================= Fast Scanner ================= */
+
+    static class FastScanner {
+
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+
+        private int ptr = 0;
+        private int len = 0;
+
+        private int read() {
+            if (ptr >= len) {
+                ptr = 0;
+
+                try {
+                    len = in.read(buffer);
+                } catch (IOException e) {
+                    return -1;
+                }
+
+                if (len <= 0) {
+                    return -1;
+                }
+            }
+
+            return buffer[ptr++];
+        }
+
+        int nextInt() {
+            int c;
+
+            while ((c = read()) <= ' ') {
+                if (c == -1) {
+                    return -1;
+                }
+            }
+
+            int sign = 1;
+
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+
+            return val * sign;
+        }
+
+        long nextLong() {
+            int c;
+
+            while ((c = read()) <= ' ') {
+                if (c == -1) {
+                    return -1;
+                }
+            }
+
+            int sign = 1;
+
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            long val = 0;
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+
+            return val * sign;
+        }
+
+        String next() {
+            int c;
+
+            while ((c = read()) <= ' ') {
+                if (c == -1) {
+                    return null;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (c > ' ') {
+                sb.append((char) c);
+                c = read();
+            }
+
+            return sb.toString();
+        }
+    }
+}
