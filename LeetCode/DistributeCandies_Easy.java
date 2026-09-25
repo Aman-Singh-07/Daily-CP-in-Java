238c74220477db6bb15f6c225aab4f59498dd96e
Aman Singh <amansingh17112004@gmail.com>
2026-09-25 21:25:27 +0530

    Create DistributeCandies_Easy.java

---
+++ LeetCode/DistributeCandies_Easy.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/distribute-candies/
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:candyType){
+            set.add(num);
+        }
+        return Math.min(set.size(),candyType.length/2);
+    }
+}
